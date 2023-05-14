a1b3f5d229f885edfb6bb1a058db3b4ad98107a4
Jorge Jacuinde Mayés <jjmayes066@gmail.com>
2023-05-14 13:02:23 -0600

    Update review_range_calculator.py

--- acquisition/review_range_calculator.py
+++ acquisition/review_range_calculator.py
@@ -1,16 +1,15 @@
-datos = list(map(int,input().split()))
+def calc(datos):
+    resultado = (datos[0] * 100)/(datos[0] + datos[1])
 
-resultado = (datos[0] * 100)/(datos[0] + datos[1])
+    if 0 <= resultado <= 20:
+        resultado = 1
+    elif 20 < resultado <= 40:
+        resultado = 2
+    elif 40 < resultado <= 60:
+        resultado = 3
+    elif 60 < resultado <= 80:
+        resultado = 4
+    elif 80 < resultado <= 100:
+        resultado = 5
 
-if 0 <= resultado <= 20:
-    resultado = 1
-elif 20 < resultado <= 40:
-    resultado = 2
-elif 40 < resultado <= 60:
-    resultado = 3
-elif 60 < resultado <= 80:
-    resultado = 4
-elif 80 < resultado <= 100:
-    resultado = 5
-
-print(resultado)
+    return(resultado)
