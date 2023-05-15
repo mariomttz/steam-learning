8c12909b7f3672f881dd622f7a20b365b3bbd1bd
Mario Martinez <mariomttz@comunidad.unam.mx>
2023-05-14 18:38:33 -0600

    Many files and permissions added

--- acquisition/review.py
+++ acquisition/review.py
@@ -1,15 +1,22 @@
-def calc(datos):
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+def rangeCalculator(datos: list) -> int:
     resultado = (datos[0] * 100)/(datos[0] + datos[1])
 
     if 0 <= resultado <= 20:
         resultado = 1
+
     elif 20 < resultado <= 40:
         resultado = 2
+
     elif 40 < resultado <= 60:
         resultado = 3
+
     elif 60 < resultado <= 80:
         resultado = 4
+
     elif 80 < resultado <= 100:
         resultado = 5
 
-    return(resultado)
+    return resultado
