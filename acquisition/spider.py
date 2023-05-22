c2396176f00d8c8e056b1fbbb10db8777be357da
Mario Martinez <mariomttz@comunidad.unam.mx>
2023-05-22 11:19:03 -0600

    Spider update

--- acquisition/spider.py
+++ acquisition/spider.py
@@ -2,37 +2,90 @@
 # -*- coding: utf-8 -*-
 
 # Libraries
+from cleaner import cleaner_data
 from time import sleep
+import db_config
+import datetime
 import requests
 import mariadb
+import json
 import sys
 
 # Functions
-def main():
-    # Source = SteamSpy API, Method = GET, Tag = Indie
-    # url = 'https://steamspy.com/api.php?request=tag&tag=Indie'
+# Spider main function 
+def main(url: str):
+    # DB connector settings
+    # References https://mariadb.com/resources/blog/how-to-connect-python-programs-to-mariadb/
+    # For this part of the code
 
-    url = 'https://steamspy.com/api.php?request=appdetails&appid=730'
+    #Connect to MariaDB
+    try:
+        conn = mariadb.connect(
+            user = db_config.setting['user'],
+            password = db_config.setting['password'],
+            host = db_config.setting['host'],
+            port = db_config.setting['port'],
+            database = db_config.setting['database']
+        )
+
+    except mariadb.Error as e:
+        sys.exit(1)
+
+    # Get the cursor
+    cur = conn.cursor()
+
+    # End part of code
 
     # Number of attempts to get the data
     attempts = 10
 
     # Check the response of the get function
-    response_get_data = get_dat(url, attempts)
+    response_get_data = get_data(url, attempts)
 
     # We finish the spider if we do not have data
     if response_get_data == None:
         return
 
     # If we have data, we assign it to the data variable
-    data = response_get_data
+    steam_data = response_get_data
 
+    # We get the current day
+    date = datetime.datetime.now().strftime('%Y-%m-%d')
 
-def get_dat(url: str, attempts = 5):
+    # Iterate on the data and
+    # make the insertions in the database
+    for key, value in steam_data.items():
+        sql_instruction = cleaner_data('value', date)
+
+        # References https://mariadb.com/resources/blog/how-to-connect-python-programs-to-mariadb/
+        # For this part of the code
+
+        # Insert the information
+        try:
+            cursor.execute(sql_cursor)
+
+        except mariadb.Error as e:
+            continue
+
+        # End part of code
+    
+    # Commited the insertions on the db
+    conn.commit()
+
+    # Finishing the spider
+    # Close Connection
+    conn.close()
+    return
+
+# Function to get data from the API
+def get_data(url: str, attempts = 5):
     response = requests.get(url)
     status_code = response.status_code
 
     for attempt in range(attempts):
+
+        # References https://oxylabs.io/blog/python-requests
+        # For this part of the code
         if status_code == 200:
             data = response.json()
             return data
@@ -41,8 +94,14 @@ def get_dat(url: str, attempts = 5):
             sleep(90)
             response = requests.get(url)
             status_code = response.status_code
+
+        # End part of code
     
     return
 
 if __name__ == '__main__':
-    main()
+    # Source = SteamSpy API, Method = GET, Tag = Indie
+    # url = 'https://steamspy.com/api.php?request=tag&tag=Indie'
+    url = 'https://steamspy.com/api.php?request=tag&tag=Hardware'
+
+    main(url)
