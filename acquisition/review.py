8e2b96692d69a123db7c3f0102d4b329c575110a
Mario Martinez <mariomttz@comunidad.unam.mx>
2023-05-21 23:01:24 -0600

    Files update

--- acquisition/review.py
+++ acquisition/review.py
@@ -1,7 +1,7 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-def rangeCalculator(datos: list) -> int:
+def range_calculator(datos: list) -> int:
     resultado = (datos[0] * 100)/(datos[0] + datos[1])
 
     if 0 <= resultado <= 20:
--- acquisition/spider.py
+++ acquisition/spider.py
@@ -1,2 +1,48 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+# Libraries
+from time import sleep
+import requests
+import mariadb
+import sys
+
+# Functions
+def main():
+    # Source = SteamSpy API, Method = GET, Tag = Indie
+    # url = 'https://steamspy.com/api.php?request=tag&tag=Indie'
+
+    url = 'https://steamspy.com/api.php?request=appdetails&appid=730'
+
+    # Number of attempts to get the data
+    attempts = 10
+
+    # Check the response of the get function
+    response_get_data = get_dat(url, attempts)
+
+    # We finish the spider if we do not have data
+    if response_get_data == None:
+        return
+
+    # If we have data, we assign it to the data variable
+    data = response_get_data
+
+
+def get_dat(url: str, attempts = 5):
+    response = requests.get(url)
+    status_code = response.status_code
+
+    for attempt in range(attempts):
+        if status_code == 200:
+            data = response.json()
+            return data
+
+        else:
+            sleep(90)
+            response = requests.get(url)
+            status_code = response.status_code
+    
+    return
+
+if __name__ == '__main__':
+    main()
